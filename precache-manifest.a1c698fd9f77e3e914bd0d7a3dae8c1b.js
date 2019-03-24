5d4f45bbc5a81f799c719404826efa02e2b88204
Smallp Tsai <smallptsai@gmail.com>
2019-03-24 13:46:41 +0800

    Updates

---
+++ precache-manifest.a1c698fd9f77e3e914bd0d7a3dae8c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d1760bcca031bca2a30",
+    "url": "/hexapod-web/static/css/main.993b054c.chunk.css"
+  },
+  {
+    "revision": "2d1760bcca031bca2a30",
+    "url": "/hexapod-web/static/js/main.2cd0b825.chunk.js"
+  },
+  {
+    "revision": "93fa880413d7f33adca3",
+    "url": "/hexapod-web/static/js/runtime~main.d2b03934.js"
+  },
+  {
+    "revision": "a6104a9fc72db049ae2a",
+    "url": "/hexapod-web/static/js/2.bb1a2f1e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hexapod-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "710024f209d129a129e28b45829869fb",
+    "url": "/hexapod-web/index.html"
+  }
+];
